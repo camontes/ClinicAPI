1b476ca2ef156042aec21dc438c6a6f59ccfd052
camontes <pope624@hotmail.com>
2020-06-13 12:11:21 -0500

    Add CreateMedicalAppointmentCommand and MedicalAppointmentViewModel

---
+++ src/SampleAPI/Commands/CreateMedicalAppointmentCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.Commands
+{
+    public class CreateMedicalAppointmentCommand
+    {
+        public string Description { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string Username { get; set; }
+        public int TypeMedicalAppointmentId { get; set; }
+    }
+}
---
+++ src/SampleAPI/ViewModels/MedicalAppointmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAPI.ViewModels
+{
+    public class MedicalAppointmentViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string Username { get; set; }
+        public int TypeMedicalAppointmentId { get; set; }
+        public string TypeMedicalAppointmentName { get; set; }
+    }
+}
