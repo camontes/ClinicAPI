46a991fd32f5b4e8d50a497e9de8a9d5dddc2937
camontes <pope624@hotmail.com>
2020-06-13 17:41:08 -0500

    Create Unit Test to MedicalAppointment

--- test/SampleAPI.Tests/Common/Endpoints.cs
+++ test/SampleAPI.Tests/Common/Endpoints.cs
@@ -6,10 +6,15 @@ namespace SampleAPI.Tests.Common
 {
     static class Endpoints
     {
+        //user
         public const string USERS = "/api/users";
 
         public const string GETUSER = "/api/users/ByUsername";
 
         public const string VALIDATE = "/api/users/ValidateCredentiales";
+
+        //MedicalAppointment
+        public const string MEDICALAPPOINTMENTS = "/api/MedicalAppointments";
+        public const string BYUSERNAME = "/api/users/ByUsername";
     }
 }
---
+++ test/SampleAPI.Tests/MedicalAppointmentControllerFunctionalTest.cs
@@ -0,0 +1,92 @@
+﻿using SampleAPI.Commands;
+using SampleAPI.Tests.Common;
+using SampleAPI.Tests.Util;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SampleAPI.Tests
+{
+    public class MedicalAppointmentControllerFunctionalTest : WebTest
+    {
+        [Fact]
+        public async Task GetAllByUsername_UnexistingUser()
+        {
+            // Execute
+            var response = await _client.GetAsync(
+                $"{Endpoints.BYUSERNAME}/unknown");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Check
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetById()
+        {
+            // Execute
+            var response = await _client.GetAsync(
+                $"{Endpoints.MEDICALAPPOINTMENTS}/-1");
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Check
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreateMedicalAppointment_InvalidDate()
+        {
+            // Prepare
+            var createMedicalAppointmentCommand = new CreateMedicalAppointmentCommand
+            {
+                Description = "Sample",
+                CreatedAt = DateTime.Now.AddDays(-20),
+                Username = "camontes",
+                TypeMedicalAppointmentId = 1
+            };
+
+            // Execute
+
+            var responseCreate = await _client.PostAsJsonAsync($"{Endpoints.MEDICALAPPOINTMENTS}", createMedicalAppointmentCommand);
+
+            Assert.Equal(HttpStatusCode.BadRequest, responseCreate.StatusCode);
+
+        }
+
+        [Fact]
+        public async Task CreateMedicalAppointment_InvalidUsername()
+        {
+            // Prepare
+            var createMedicalAppointmentCommand = new CreateMedicalAppointmentCommand
+            {
+                Description = "Sample",
+                CreatedAt = DateTime.Now.AddDays(15),
+                Username = "unknown",
+                TypeMedicalAppointmentId = 1
+            };
+
+            // Execute
+
+            var responseCreate = await _client.PostAsJsonAsync($"{Endpoints.MEDICALAPPOINTMENTS}", createMedicalAppointmentCommand);
+
+            Assert.Equal(HttpStatusCode.NotFound, responseCreate.StatusCode);
+
+        }
+
+
+        [Fact]
+        public async Task CancelMedicalAppointment()
+        {
+
+            // Execute
+            var responseDelete = await _client.DeleteAsync($"{Endpoints.MEDICALAPPOINTMENTS}/-300");
+
+            Assert.Equal(HttpStatusCode.NotFound, responseDelete.StatusCode);
+
+        }
+    }
+}
