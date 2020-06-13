dcd966c423cd4fb373823f9e45176e80383cd6c0
camontes <pope624@hotmail.com>
2020-06-13 09:59:52 -0500

    Create User model and CRUD

---
+++ src/SampleAPI/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleAPI.Domain;
+using SampleAPI.Migrations.Data;
+
+namespace SampleAPI.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+    }
+}
---
+++ src/SampleAPI/Infrastructure/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SampleAPI.Domain.Infrastructure;
+using SampleAPI.Domain;
+
+namespace SampleAPI.Infrastructure
+{
+    public class UserRepository : IUserRepository
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }        
+
+        public async Task CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteUserAsync(User user)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
